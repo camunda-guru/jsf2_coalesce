3cbc6bb01ba1bbf7a0731d4c9d4a8d8e3c610c3c
eswaribala <Parameswaribala@gmail.com>
2017-07-11 10:34:27 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,34 @@
+package com.ness.qm.model;
+
+public class Address {
+
+	private String doorNo;
+	private String streetName;
+	private String city;
+	private String state;
+	public String getDoorNo() {
+		return doorNo;
+	}
+	public void setDoorNo(String doorNo) {
+		this.doorNo = doorNo;
+	}
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+}
---
+++ Person.java
@@ -0,0 +1,44 @@
+package com.ness.qm.model;
+
+import java.util.Date;
+
+public class Person {
+
+	private String firstName;
+	private String lastName;
+	private String Email;
+	private Date dob;
+	private Address address;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
